94a797885e40141cb27b39f389902df08d4e8e44
yanneves <hello@yanneves.com>
2020-05-16 17:11:33 +0100

    Removed unused index files in public/sounds/

--- public/sounds/floof/index.js
+++
@@ -1,8 +0,0 @@
-export { default } from "./uwuu.ogg"
-export { default as angi_woof } from "./angi_woof.ogg"
-export { default as awoof } from "./awoof.ogg"
-export { default as close_uwuu } from "./close_uwuu.ogg"
-export { default as distant_uwuu } from "./distant_uwuu.ogg"
-export { default as floof_growl } from "./floof_growl.ogg"
-export { default as more_distant_uwuu } from "./more_distant_uwuu.ogg"
-export { default as more_uwuu } from "./more_uwuu.ogg"
--- public/sounds/index.js
+++
@@ -1,9 +0,0 @@
-import * as bear from "./bear/"
-import * as floof from "./floof/"
-import * as wannabe from "./wannabe/"
-
-export default {
-  bear,
-  floof,
-  wannabe
-}
